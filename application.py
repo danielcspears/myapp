f96a45f64c1eff2c4021c9fff9a40688e91fb872
Daniel Spears <danielspears@gmail.com>
2018-03-30 14:34:52 -0500

    another

--- application.py
+++ application.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask , render_template
+import datetime
+
 application = Flask(__name__)
 
 @application.route("/", methods=["GET"])
 
 def hello():
- return "<p>Hello world</p>"
+	now = datetime.datetime.now()
+	timeString = now.strftime("%Y-%m-%d %H:%M")
+	templateData = {
+	"title" : "Daniel C Spears - Flask on AWS elastic beanstalk - deployed on 3-30-2018",
+	"time": timeString
+	}
+	return render_template("main.html", **templateData)
+
 
 if __name__ == "__main__":
  application.run()
