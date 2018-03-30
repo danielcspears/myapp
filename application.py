477c2a84358ede21272a80369aa8cbe5a379be0b
Daniel Spears <danielspears@gmail.com>
2018-03-30 13:48:42 -0500

    Commit messagege

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from flask import Flask
 application = Flask(__name__)
 
-@application.route('/', methods=['GET'])
+@application.route("/", methods=["GET"])
 
 def hello():
- return '<p>Hello world</p>'
+ return "<p>Hello world</p>"
 
 if __name__ == "__main__":
  application.run()
